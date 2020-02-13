2355e41edb216a0164f023292fcefb5c6753ace9
carlos9m9 <46663157+carlos9m9@users.noreply.github.com>
2020-02-13 18:22:10 -0500

    First Test - No methods

--- melo_problem1/src/melo_problem1/CAI_1.java
+++ melo_problem1/src/melo_problem1/CAI_1.java
@@ -1,5 +1,29 @@
 package melo_problem1;
 
-public class CAI_1 {
+import java.security.SecureRandom;
+import java.util.Scanner;
 
+public class CAI_1 {
+	
+	
+	public static void main (String[]args) {
+		Scanner s = new Scanner(System.in);
+		SecureRandom r = new SecureRandom();
+		int num1 = r.nextInt(9);
+		int num2 = r.nextInt(9);
+		
+		
+		int i=0;
+		while(i==0) {
+			System.out.println("How much is "+num1+" times "+num2+"?" );
+			int answer = s.nextInt();
+			if(answer == (num1*num2)) {
+				System.out.println("Very good!");
+				i++;
+			}else{
+				System.out.println("No, please try again.");
+			}
+		}
+		s.close();
+	}
 }
