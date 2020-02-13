cbd430140a743af6b8ab17f86346eee9305bc145
carlos9m9 <46663157+carlos9m9@users.noreply.github.com>
2020-02-13 18:59:22 -0500

    Added methods and comments

--- melo_problem1/src/melo_problem1/CAI_1.java
+++ melo_problem1/src/melo_problem1/CAI_1.java
@@ -4,26 +4,53 @@ import java.security.SecureRandom;
 import java.util.Scanner;
 
 public class CAI_1 {
+	//Declaration of numbers to use with the SecureRandom object
+	Scanner s = new Scanner(System.in);
+	private SecureRandom sr = new SecureRandom();
+	private int num1 = sr.nextInt(9);
+	private int num2 = sr.nextInt(9);
 	
-	
+	//Main method
 	public static void main (String[]args) {
-		Scanner s = new Scanner(System.in);
-		SecureRandom r = new SecureRandom();
-		int num1 = r.nextInt(9);
-		int num2 = r.nextInt(9);
-		
-		
-		int i=0;
-		while(i==0) {
-			System.out.println("How much is "+num1+" times "+num2+"?" );
-			int answer = s.nextInt();
-			if(answer == (num1*num2)) {
-				System.out.println("Very good!");
-				i++;
-			}else{
-				System.out.println("No, please try again.");
-			}
+		CAI_1 c1 = new CAI_1();
+		c1.quiz();
+	}
+
+	/*Quiz method asks the question through askQuestion, receives a response through readResponse()
+	and passes said response into the isAnswerCorrect method*/
+	void quiz() {
+		askQuestion();
+		int ans = readResponse();
+		isAnswerCorrect(ans);
+	}
+	
+	void askQuestion() {
+		System.out.println("How much is "+num1+" times "+num2+"?" );
+	}
+	
+	private int readResponse() {
+		int ans = s.nextInt();
+		return ans;
+	}
+	
+	//Simple if else statement to decide whether the answer was correct or not
+	void isAnswerCorrect(int ans) {
+		if(ans==(num1*num2)) {
+			displayCorrectResponse();
+		}else {
+			displayIncorrectResponse();
 		}
-		s.close();
+		
+	}
+	
+	void displayCorrectResponse() {
+		System.out.println("Very good!");
+	}
+	
+	void displayIncorrectResponse() {
+		System.out.println("No, please try again.");
+		/*If the answer is incorrect we call the method quiz()
+		to start back from the moment the question was asked*/
+		quiz();
 	}
 }
