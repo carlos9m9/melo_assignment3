93518e854af0b19c0c42c65779a21a9922cf9444
carlos9m9 <46663157+carlos9m9@users.noreply.github.com>
2020-02-17 13:43:15 -0500

    Working program

--- melo_problem2/src/melo_problem2/SavingsAccount.java
+++ melo_problem2/src/melo_problem2/SavingsAccount.java
@@ -1,5 +1,25 @@
 package melo_problem2;
 
 public class SavingsAccount {
-
+	private static double annualInterestRate;
+	private double savingBalance;
+	private double monthlyInterest;
+	
+	public SavingsAccount(double balance) {
+		savingBalance = balance;
+	}
+	
+	public static void modifyInterestRate(double newAnnual) {
+		annualInterestRate = newAnnual;
+	}
+	
+	public void calculateMonthlyInterest() {
+		monthlyInterest = (savingBalance * annualInterestRate)/12;
+		System.out.printf("Monthly interest: $%.2f\r\n",monthlyInterest);
+	}
+	
+	public void calculateBalance() {
+		savingBalance += monthlyInterest;
+		System.out.printf("Your balance is: $%.2f\r\n\r\n",savingBalance);
+	}
 }
--- melo_problem2/src/melo_problem2/SavingsAccountTest.java
+++ melo_problem2/src/melo_problem2/SavingsAccountTest.java
@@ -3,8 +3,25 @@ package melo_problem2;
 public class SavingsAccountTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		SavingsAccount saver1 = new SavingsAccount(2000.00);
+		SavingsAccount saver2 = new SavingsAccount(3000.00);
+		
+		SavingsAccount.modifyInterestRate(4);
+		
+		saver1.calculateMonthlyInterest();
+		saver1.calculateBalance();
+		
+		saver2.calculateMonthlyInterest();
+		saver2.calculateBalance();
+		
+		SavingsAccount.modifyInterestRate(5);
+		
+		saver1.calculateMonthlyInterest();
+		saver1.calculateBalance();
+		
+		saver2.calculateMonthlyInterest();
+		saver2.calculateBalance();
+		
 	}
 
 }
